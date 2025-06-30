7a64e18246515cb36385933b512a4735df348a3e
Kawser420 <muhammadkawser202@gmail.com>
2025-06-30 06:12:19 +0600

    logger error message handle

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log(process);
+const path = require("path");
+
+const inputArguments = process.argv.slice(2);
+
+const text = inputArguments.join(" ");
+
+if (!text) {
+  console.log("Please provide a message to log");
+  console.log("Example : node js Hello Word!!!");
+  process.exit(1);
+}
+
+const filePath = path.join(__dirname, "log.txt");
+console.log(filePath);
